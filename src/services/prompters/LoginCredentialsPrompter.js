9def6b4cb2e97c01a622303962b64274d589295c
jaebradley <jae.b.bradley@gmail.com>
2017-12-20 22:29:44 -0500

    refactor(credentials-prompter): add separate login credentials prompter

---
+++ src/services/prompters/LoginCredentialsPrompter.js
@@ -0,0 +1,44 @@
+import inquirer from 'inquirer';
+
+class LoginCredentialsPrompter {
+  constructor(userService) {
+    this.userService = userService;
+  }
+
+  async prompt() {
+    return inquirer.prompt([
+      {
+        name: 'username',
+        message: 'Input your GitHub Username',
+        validate: this.validateUsername,
+        type: 'input',
+      },
+      {
+        name: 'password',
+        message: 'Input your GitHub password',
+        validate: LoginCredentialsPrompter.validatePassword,
+        type: 'password',
+      },
+    ]);
+  }
+
+  async validateUsername(username) {
+    const usernameExists = await this.userService.usernameExists(username);
+
+    if (usernameExists) {
+      return true;
+    }
+
+    return `${username} is not a valid username`;
+  }
+
+  static validatePassword(password) {
+    if (password && password.length > 0) {
+      return true;
+    }
+
+    return 'Please enter a valid password';
+  }
+}
+
+export default LoginCredentialsPrompter;
