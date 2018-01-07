bb2750d019b2e02e1d0a7b9843c273eb8397a758
jaebradley <jae.b.bradley@gmail.com>
2018-01-07 10:21:52 -0500

    test(fragment-styler): add test

---
+++ src/services/code/FragmentStyler.js
@@ -0,0 +1,22 @@
+import chalk from 'chalk';
+import {
+  TextStyler,
+  StyledRange,
+  TextStyle,
+  TextColor,
+  TextFormat,
+} from 'textstyler';
+
+const getStyledRanges = ranges => ranges
+  .map(range => new StyledRange(range, new TextStyle(TextColor.RED, null, TextFormat.BOLD)));
+
+const styleFragmentText = (fragment) => {
+  const { matchingRanges, text } = fragment;
+  const styledRanges = getStyledRanges(matchingRanges);
+  const styledText = TextStyler.style(text, styledRanges);
+  return styledText.replace(/\r?\n?/g, '');
+};
+
+const styleFragment = ({ filePath, repositoryName, fragment }) => `${chalk.magenta.bold(filePath)} in ${chalk.bold.yellow(repositoryName)}\n${styleFragmentText(fragment)}`;
+
+export { getStyledRanges, styleFragmentText, styleFragment };
